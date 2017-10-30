ae272b9048d6dd14899c54d4554d894fe7f28267
Bpham1 <brandon.pham@utexas.edu>
2017-10-30 10:10:59 -0500

    A working map generator

--- Rogue Like/Assets/Scripts/GameManager.cs
+++ Rogue Like/Assets/Scripts/GameManager.cs
@@ -4,12 +4,19 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour {
 
+	public static GameManager instance = null;
 	public BoardManager boardScript;
 
 	private int level = 3;
 
 	// Use this for initialization
 	void Awake () {
+		if (instance == null)
+			instance = this;
+		else if (instance != this)
+			Destroy (gameObject);
+
+		DontDestroyOnLoad (gameObject);
 		boardScript = GetComponent<BoardManager> ();
 		InitGame();
 	}
